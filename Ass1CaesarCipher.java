647cdfeeb2ca189034b7b9ca46214203b71d017c
Abhishek Gaikwad <122622440+Abhishekgaikwad01@users.noreply.github.com>
2024-07-30 14:29:35 +0530

    Update Ass1CaesarCipher.java

--- Ass1CaesarCipher.java
+++ Ass1CaesarCipher.java
@@ -13,7 +13,22 @@ public class Ass1CaesarCipher{
         }   
         return encryptStr;   
     }   
-        
+    public static String decryptData(String inputStr, int Key)   
+    {    
+        String decryptStr = "";   
+          
+        for (int i = 0; i < inputStr.length(); i++)   
+        {   
+            int pos = ALPHABET.indexOf(inputStr.charAt(i));   
+            int decryptPos = (pos - Key) % 26;   
+            if (decryptPos < 0){   
+                decryptPos = ALPHABET.length() + decryptPos;   
+            }   
+            char decryptChar = ALPHABET.charAt(decryptPos);   
+            decryptStr += decryptChar;   
+        }   
+        return decryptStr;   
+    }    
     public static void main(String[] args)   {   
         Scanner sc = new Scanner(System.in);   
             
@@ -22,8 +37,11 @@ public class Ass1CaesarCipher{
           
         System.out.println("Enter the value of Key :");   
         int Key = Integer.valueOf(sc.nextLine());   
-          
-        System.out.println("Encrypted Data :"+encryptData(inputStr, Key));   
+        
+        String s  = encryptData(inputStr, Key);
+        System.out.println("Encrypted Data :"+s);   
+        System.out.println("Decrypted Data :"+decryptData(s, Key));     
+
         sc.close();   
     }   
 }   
